0fd2e7c6c1398a46ac7e7b711d002ac64b7ba965
jollejonas <jollevb@hotmail.com>
2024-12-20 00:33:40 +0100

    Project completed.

---
+++ Movies.jollejonas/Movies.jollejonas/Data/MoviesjollejonasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movies.jollejonas.Models;
+
+namespace Movies.jollejonas.Data
+{
+    public class MoviesjollejonasContext : DbContext
+    {
+        public MoviesjollejonasContext (DbContextOptions<MoviesjollejonasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies.jollejonas.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Movies.jollejonas/Movies.jollejonas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movies.jollejonas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
